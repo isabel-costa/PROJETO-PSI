c3ae81c78952ef255ed01277e24cb08f7716adb2
Isabel Costa <146230706+isabel-costa@users.noreply.github.com>
2025-11-05 22:24:48 +0000

    Correções

--- Web App/SisMedCare/vendor/hail812/yii2-adminlte3/src/views/layouts/sidebar.php
+++ Web App/SisMedCare/vendor/hail812/yii2-adminlte3/src/views/layouts/sidebar.php
@@ -1,56 +1,82 @@
-<?php
-use yii\helpers\Url;
-use yii\helpers\Html;
-
-$user = Yii::$app->user->identity;
-$username = $user ? Html::encode($user->username) : 'Visitante';
-?>
-
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="<?= Url::home() ?>" class="brand-link">
-        <img src="<?= $assetDir ?>/img/AdminLTELogo.png"
-             alt="Logo"
-             class="brand-image img-circle elevation-3"
-             style="opacity:.8">
-        <span class="brand-text font-weight-light">SisMedCare</span>
+    <a href="index3.html" class="brand-link">
+        <img src="<?=$assetDir?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
-        <!-- Painel do utilizador -->
+        <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="<?= $assetDir ?>/img/user2-160x160.jpg"
-                     class="img-circle elevation-2"
-                     alt="User Image">
+                <img src="<?=$assetDir?>/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a class="d-block"><?= $username ?></a>
+                <a href="#" class="d-block">Alexander Pierce</a>
             </div>
         </div>
 
-        <!-- Menu -->
+        <!-- SidebarSearch Form -->
+        <!-- href be escaped -->
+        <!-- <div class="form-inline">
+            <div class="input-group" data-widget="sidebar-search">
+                <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+                <div class="input-group-append">
+                    <button class="btn btn-sidebar">
+                        <i class="fas fa-search fa-fw"></i>
+                    </button>
+                </div>
+            </div>
+        </div> -->
+
+        <!-- Sidebar Menu -->
         <nav class="mt-2">
-            <?= \hail812\adminlte\widgets\Menu::widget([
+            <?php
+            echo \hail812\adminlte\widgets\Menu::widget([
                 'items' => [
                     [
-                        'label' => 'Médicos',
-                        'icon'  => 'user-md',
-                        'url'   => ['medicos/index'],
+                        'label' => 'Starter Pages',
+                        'icon' => 'tachometer-alt',
+                        'badge' => '<span class="right badge badge-info">2</span>',
+                        'items' => [
+                            ['label' => 'Active Page', 'url' => ['site/index'], 'iconStyle' => 'far'],
+                            ['label' => 'Inactive Page', 'iconStyle' => 'far'],
+                        ]
                     ],
+                    ['label' => 'Simple Link', 'icon' => 'th', 'badge' => '<span class="right badge badge-danger">New</span>'],
+                    ['label' => 'Yii2 PROVIDED', 'header' => true],
+                    ['label' => 'Login', 'url' => ['site/login'], 'icon' => 'sign-in-alt', 'visible' => Yii::$app->user->isGuest],
+                    ['label' => 'Gii',  'icon' => 'file-code', 'url' => ['/gii'], 'target' => '_blank'],
+                    ['label' => 'Debug', 'icon' => 'bug', 'url' => ['/debug'], 'target' => '_blank'],
+                    ['label' => 'MULTI LEVEL EXAMPLE', 'header' => true],
+                    ['label' => 'Level1'],
                     [
-                        'label' => 'Secretárias',
-                        'icon'  => 'user-tie',
-                        'url'   => ['secretarias/index'],
+                        'label' => 'Level1',
+                        'items' => [
+                            ['label' => 'Level2', 'iconStyle' => 'far'],
+                            [
+                                'label' => 'Level2',
+                                'iconStyle' => 'far',
+                                'items' => [
+                                    ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle'],
+                                    ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle'],
+                                    ['label' => 'Level3', 'iconStyle' => 'far', 'icon' => 'dot-circle']
+                                ]
+                            ],
+                            ['label' => 'Level2', 'iconStyle' => 'far']
+                        ]
                     ],
-                    [
-                        'label' => 'Medicamentos',
-                        'icon'  => 'pills',
-                        'url'   => ['medicamento/index'],
-                    ],
+                    ['label' => 'Level1'],
+                    ['label' => 'LABELS', 'header' => true],
+                    ['label' => 'Important', 'iconStyle' => 'far', 'iconClassAdded' => 'text-danger'],
+                    ['label' => 'Warning', 'iconClass' => 'nav-icon far fa-circle text-warning'],
+                    ['label' => 'Informational', 'iconStyle' => 'far', 'iconClassAdded' => 'text-info'],
                 ],
-            ]) ?>
+            ]);
+            ?>
         </nav>
+        <!-- /.sidebar-menu -->
     </div>
+    <!-- /.sidebar -->
 </aside>
--- Web App/SisMedCare/vendor/hail812/yii2-adminlte3/src/views/site/index.php
+++ Web App/SisMedCare/vendor/hail812/yii2-adminlte3/src/views/site/index.php
@@ -1,86 +1,133 @@
 <?php
-$this->title = 'Painel de Administração';
+$this->title = 'Starter Page';
 $this->params['breadcrumbs'] = [['label' => $this->title]];
 ?>
 <div class="container-fluid">
-
-    <div class="row mb-4">
-        <div class="col-lg-12">
-            <?= \hail812\adminlte\widgets\Alert::widget([
-                'type' => 'info',
-                'body' => '
-                    <h3>Painel de Administração</h3>
-                    <p>Bem-vindo à área de administração do sistema. 
-                    Aqui pode gerir <strong>médicos</strong>, <strong>secretárias</strong> e <strong>medicação</strong>, 
-                    bem como consultar estatísticas e informações do funcionamento da clínica.</p>
-                ',
-            ]) ?>
-        </div>
-    </div>
-
-    <!-- Mantém os widgets, mas mais abaixo -->
     <div class="row">
         <div class="col-lg-6">
-            <?= \hail812\adminlte\widgets\Callout::widget([
+            <?= \hail812\adminlte\widgets\Alert::widget([
                 'type' => 'success',
-                'head' => 'Dica Rápida',
-                'body' => 'Utilize o menu lateral para aceder às secções de gestão e configurações do sistema.'
+                'body' => '<h3>Congratulations!</h3>',
+            ]) ?>
+            <?= \hail812\adminlte\widgets\Callout::widget([
+                'type' => 'danger',
+                'head' => 'I am a danger callout!',
+                'body' => 'There is a problem that we need to fix. A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.'
             ]) ?>
         </div>
     </div>
 
-    <!-- Widgets de informação (mantidos para futuro uso) -->
-    <div class="row mt-4">
+    <div class="row">
         <div class="col-12 col-sm-6 col-md-3">
             <?= \hail812\adminlte\widgets\InfoBox::widget([
-                'text' => 'Médicos registados',
-                'number' => '12',
-                'icon' => 'fas fa-user-md',
-                'theme' => 'primary',
-            ]) ?>
-        </div>
-        <div class="col-md-4 col-sm-6 col-12">
-            <?= \hail812\adminlte\widgets\InfoBox::widget([
-                'text' => 'Secretárias registadas',
-                'number' => '4',
-                'theme' => 'info',
-                'icon' => 'fas fa-user-tie',
-            ]) ?>
-        </div>
-        <div class="col-md-4 col-sm-6 col-12">
-            <?= \hail812\adminlte\widgets\InfoBox::widget([
-                'text' => 'Medicamentos registados',
-                'number' => '128',
-                'theme' => 'gradient-success',
-                'icon' => 'fas fa-pills',
+                'text' => 'CPU Traffic',
+                'number' => '10 <small>%</small>',
+                'icon' => 'fas fa-cog',
             ]) ?>
         </div>
     </div>
 
-    <!-- Pequenos blocos decorativos -->
-    <div class="row mt-4">
-        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-            <?= \hail812\adminlte\widgets\SmallBox::widget([
-                'title' => '3',
-                'text' => 'Pedidos pendentes',
-                'icon' => 'fas fa-calendar-check',
-                'theme' => 'warning',
-            ]) ?>
-        </div>
-        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-            <?= \hail812\adminlte\widgets\SmallBox::widget([
-                'title' => '8',
-                'text' => 'Novas mensagens',
+    <div class="row">
+        <div class="col-md-4 col-sm-6 col-12">
+            <?= \hail812\adminlte\widgets\InfoBox::widget([
+                'text' => 'Messages',
+                'number' => '1,410',
                 'icon' => 'far fa-envelope',
-                'theme' => 'gradient-info',
+            ]) ?>
+        </div>
+        <div class="col-md-4 col-sm-6 col-12">
+            <?= \hail812\adminlte\widgets\InfoBox::widget([
+                'text' => 'Bookmarks',
+                'number' => '410',
+                 'theme' => 'success',
+                'icon' => 'far fa-flag',
+            ]) ?>
+        </div>
+        <div class="col-md-4 col-sm-6 col-12">
+            <?= \hail812\adminlte\widgets\InfoBox::widget([
+                'text' => 'Uploads',
+                'number' => '13,648',
+                'theme' => 'gradient-warning',
+                'icon' => 'far fa-copy',
+            ]) ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4 col-sm-6 col-12">
+            <?= \hail812\adminlte\widgets\InfoBox::widget([
+                'text' => 'Bookmarks',
+                'number' => '41,410',
+                'icon' => 'far fa-bookmark',
+                'progress' => [
+                    'width' => '70%',
+                    'description' => '70% Increase in 30 Days'
+                ]
+            ]) ?>
+        </div>
+        <div class="col-md-4 col-sm-6 col-12">
+            <?php $infoBox = \hail812\adminlte\widgets\InfoBox::begin([
+                'text' => 'Likes',
+                'number' => '41,410',
+                'theme' => 'success',
+                'icon' => 'far fa-thumbs-up',
+                'progress' => [
+                    'width' => '70%',
+                    'description' => '70% Increase in 30 Days'
+                ]
+            ]) ?>
+            <?= \hail812\adminlte\widgets\Ribbon::widget([
+                'id' => $infoBox->id.'-ribbon',
+                'text' => 'Ribbon',
+            ]) ?>
+            <?php \hail812\adminlte\widgets\InfoBox::end() ?>
+        </div>
+        <div class="col-md-4 col-sm-6 col-12">
+            <?= \hail812\adminlte\widgets\InfoBox::widget([
+                'text' => 'Events',
+                'number' => '41,410',
+                'theme' => 'gradient-warning',
+                'icon' => 'far fa-calendar-alt',
+                'progress' => [
+                    'width' => '70%',
+                    'description' => '70% Increase in 30 Days'
+                ],
+                'loadingStyle' => true
+            ]) ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+            <?= \hail812\adminlte\widgets\SmallBox::widget([
+                'title' => '150',
+                'text' => 'New Orders',
+                'icon' => 'fas fa-shopping-cart',
             ]) ?>
         </div>
         <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+            <?php $smallBox = \hail812\adminlte\widgets\SmallBox::begin([
+                'title' => '150',
+                'text' => 'New Orders',
+                'icon' => 'fas fa-shopping-cart',
+                'theme' => 'success'
+            ]) ?>
+            <?= \hail812\adminlte\widgets\Ribbon::widget([
+                'id' => $smallBox->id.'-ribbon',
+                'text' => 'Ribbon',
+                'theme' => 'warning',
+                'size' => 'lg',
+                'textSize' => 'lg'
+            ]) ?>
+            <?php \hail812\adminlte\widgets\SmallBox::end() ?>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
             <?= \hail812\adminlte\widgets\SmallBox::widget([
-                'title' => '5',
-                'text' => 'Novos registos',
+                'title' => '44',
+                'text' => 'User Registrations',
                 'icon' => 'fas fa-user-plus',
                 'theme' => 'gradient-success',
+                'loadingStyle' => true
             ]) ?>
         </div>
     </div>
