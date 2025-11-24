3091bd1b4998cc5e5435d259c37d14e336f70a48
Isabel Costa <146230706+isabel-costa@users.noreply.github.com>
2025-11-24 19:06:01 +0000

    Corrigir controlo de acesso ao Frontend

--- Web App/SisMedCare/frontend/controllers/SiteController.php
+++ Web App/SisMedCare/frontend/controllers/SiteController.php
@@ -29,7 +29,7 @@ class SiteController extends Controller
         return [
             'access' => [
                 'class' => AccessControl::class,
-                'except' => ['login', 'index', 'contact', 'error', 'captcha', 'request-password-reset', 'reset-password'],
+                'except' => ['login', 'contact', 'error', 'captcha', 'request-password-reset', 'reset-password'],
                 'rules' => [
                     [
                         'allow' => true,
